20c7eef1830e6ad7c43586d751233d4870d664d1
zhanglei <871002990@qq.com>
2023-09-26 15:18:11 +0800

    发布测试

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img alt="hanli" class="logo" src="https://img1.baidu.com/it/u=2158171980,2545952265&fm=253&fmt=auto&app=120&f=JPEG?w=640&h=375" width="640" height="375" />
   </header>
 
   <main>
@@ -32,16 +27,6 @@ header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 }
 </style>
